eb73ffff9d6556dbb608619ecceef200515846f3
andr_ll <andr.lyt.dev@gmail.com>
2022-12-30 17:44:34 +0100

    Create build_lib.js

---
+++ build_lib.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const methodName = process.argv[2];
+const libPath = './lib';
+
+const content = `#!/usr/bin/env node
+
+require('../lib')${methodName == null ? '' : `.${methodName}`};`;
+
+if (fs.existsSync(libPath)) {
+  fs.rmSync(libPath, { recursive: true, force: true });
+}
+
+fs.mkdirSync(libPath);
+fs.writeFileSync(`${libPath}/index.js`, content);
