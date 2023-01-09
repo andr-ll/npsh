53dbb0a83992752de47b45ff026a0a3da6782e86
andr-ii <andr.lyt.dev@gmail.com>
2023-01-09 13:52:48 +0100

    feat: add jest and coverage (#8)

---
+++ test/index.spec.js
@@ -0,0 +1,26 @@
+const consoleSpy = jest.spyOn(console, 'log');
+
+describe('error handling', () => {
+  it('throw an error if not valid argument', () => {
+    expect.assertions(1);
+
+    process.argv[2] = 'foo-bar';
+
+    try {
+      require('../lib');
+    } catch (error) {
+      expect(error.message).toStrictEqual('Unsupported argument: foo-bar');
+    }
+  });
+});
+
+describe('success', () => {
+  it('run the main file', () => {
+    process.argv[2] = 'svc';
+    process.argv[3] = 'andr-ii';
+    process.argv[4] = 'npsh';
+    process.argv[5] = 'master';
+
+    require('../lib');
+  });
+});
